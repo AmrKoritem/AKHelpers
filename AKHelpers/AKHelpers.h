abd72d2913bcc6ab43ed8639ad9b42593c837fa8
Amr Koritem <amr.koritem92@gmail.com>
2023-02-14 11:56:07 +0200

    Initial Commit

---
+++ AKHelpers/AKHelpers.h
@@ -0,0 +1,18 @@
+//
+//  AKHelpers.h
+//  AKHelpers
+//
+//  Created by Amr Koritem on 14/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKHelpers.
+FOUNDATION_EXPORT double AKHelpersVersionNumber;
+
+//! Project version string for AKHelpers.
+FOUNDATION_EXPORT const unsigned char AKHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKHelpers/PublicHeader.h>
+
+
